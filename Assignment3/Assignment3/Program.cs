c7789890c12cf7058f5c849a4e3f866a33ec7c99
Brad Beattie <bbeattie@waveguide.com>
2016-02-19 13:19:37 -0500

    1st Circle Code.

---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Program
+    {
+        struct Transaction
+        {
+            public string bankName;
+            public string acctHolder;
+            public int acctNumber;
+            public string fedRefNum;
+            public decimal amount;
+            public string transDate;
+        };
+
+        static void Main()
+        {
+
+            string path = @"C:\data\ass3\";
+            string[] files = Directory.GetFiles(path);
+            List<Transaction> transactions = new List<Transaction>();
+            //List<string> parsedData = new List<string>();
+
+            foreach (string file in files)
+            {
+                if (file.StartsWith(path+"S"))
+                {
+                    try
+                    {
+
+                    }
+                    catch
+                    {
+
+                    }
+
+                }
+
+                else if (file.StartsWith(path+"W"))
+                {
+                    try
+                    {
+                        using (StreamReader readFile = new StreamReader(file))
+                        {
+                            string line;
+                            string[] row;
+                            Console.WriteLine("processing {0}", file);
+
+                            while ((line = readFile.ReadLine()) != null)
+                            {
+                                row = line.Split(',');
+                                transactions.Add(new Transaction()
+                                {
+                                    bankName = row[0],
+                                    acctHolder = row[1],
+                                    acctNumber = Convert.ToInt32(row[2]),
+                                    fedRefNum = row[4],
+                                    amount = Convert.ToDecimal(row[6]),
+                                    transDate = row[7]
+                                });
+                                Console.WriteLine(row[0]);
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                }   
+
+            }
+
+            
+            foreach (Transaction trans in transactions)
+            {
+                Console.WriteLine(trans.acctHolder);
+            }
+            Console.ReadKey();
+            
+
+        }
+    }
+}
---
+++ circle/circle/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circle
+{
+    class Program
+    {
+        static decimal PI = 3.141592654m;
+        static void Main(string[] args)
+        {
+            bool reading = true;
+            decimal circleArea;
+            decimal circleCircumference;
+
+            while (reading)
+            {
+                Console.WriteLine("Enter Radius: (? to end)");
+                string radius = Console.ReadLine();
+                if (radius == "?")
+                {
+                    reading = false;
+                    break;
+                }
+                decimal result;
+                if (decimal.TryParse(radius, out result))
+                {
+                    //compute area and circumference
+                    circleArea = computeCircleArea(result);
+                    circleCircumference = computeCircleCircumference(result);
+                    Console.WriteLine("Area = {0}", circleArea);
+                    Console.WriteLine("Circumference = {0}", circleCircumference);
+                }
+                else
+                {
+                    Console.WriteLine("LAME.");
+                }
+            }
+
+        }
+        static decimal computeCircleArea(decimal radius)
+        {
+            decimal area = Program.PI * radius * radius;
+            return area;
+        }
+        static decimal computeCircleCircumference(decimal radius)
+        {
+            decimal circum = 2.0m * Program.PI * radius;
+            return circum;
+        }
+    }
+
+}
